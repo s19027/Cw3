ba43ce4f3fb9e2daf1ff6f828f25cdffa8390fe4
s19027 <s19027@pjwstk.edu.pl>
2020-03-16 12:55:54 +0100

    init

---
+++ Cw3/WebApplication/Controllers/StudentsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            }
+
+            if (id == 2)
+            {
+                return Ok("Malewski");
+            }
+            return NotFound("Nie znaleziono studenta");
+        }
+    }
+}
